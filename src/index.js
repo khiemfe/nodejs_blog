930e5cca4d8510182d0a50a990989d741db1a2a4
Ngọc Khiêm <ngockhiem@Ngoc-Khiem-MacBook-Pro.local>
2023-09-08 20:41:03 +0700

    k

--- src/index.js
+++ src/index.js
@@ -24,8 +24,8 @@ app.use(express.json()); // submit html, hoặc sử dụng các thư viện:
 
 //Template engine
 app.engine(
-    'hbs',
-    handlebars.engine({
+                      'hbs',
+                handlebars.engine({
         extname: '.hbs', //đổi từ handlebars thành hbs cho ngắn (đổi cả đuôi .handlebars)
     }),
 );
